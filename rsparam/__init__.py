eec1e66772b407db7d12e9622307aa60afecda09
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-03-02 11:25:32 -0800

    Prepared implementation for v0.1

--- rsparam/__init__.py
+++ rsparam/__init__.py
@@ -3,32 +3,51 @@
 
 import codecs
 import csv
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+
+
+# pylama:ignore=D105
+
+# rsparam version
+__version__ = '0.1.0'
+
+
+SharedParamEntries = namedtuple('SharedParamEntries', ['groups', 'params'])
 
 
 class SharedParamFileItem(object):
     def __init__(self, lineno):
         self.lineno = lineno
 
+    def __contains__(self, key):
+        for value in self:
+            if key in value:
+                return True
+        return False
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
 
 class SharedParamGroup(SharedParamFileItem):
     def __init__(self, args, lineno=None):
         super(SharedParamGroup, self).__init__(lineno)
-        self.id = args[0]
+        self.guid = args[0]
         self.desc = args[1]
+        self.name = self.desc
 
     def __str__(self):
         return self.desc
 
     def __iter__(self):
-        return iter([self.id, self.desc])
+        return iter([self.guid, self.desc])
 
     def __repr__(self):
-        return '<{} desc:"{}" id:{}>'.format(self.__class__.__name__,
-                                             self.desc, self.id)
+        return '<{} desc:"{}" guid:{}>'.format(self.__class__.__name__,
+                                               self.desc, self.guid)
 
     def __hash__(self):
-        return self.id
+        return hash(self.guid + self.desc)
 
 
 class SharedParam(SharedParamFileItem):
@@ -56,10 +75,12 @@ class SharedParam(SharedParamFileItem):
                                                self.name, self.guid)
 
     def __hash__(self):
-        return self.guid
+        return hash(self.guid + self.name
+                    + self.datatype + self.datacategory
+                    + self.visible + self.desc + self.usermod)
 
 
-def get_params(src_file, encoding=None):
+def read_entries(src_file, encoding=None):
     # open file and collect shared param and groups
     spgroups = []
     sparams = []
@@ -78,24 +99,61 @@ def get_params(src_file, encoding=None):
     # now update sparams with group obj
     for sp in sparams:
         for spg in spgroups:
-            if sp.group == spg.id:
+            if sp.group == spg.guid:
                 sp.group = spg
 
-    return spgroups, sparams
+    return SharedParamEntries(spgroups, sparams)
 
 
-def find_duplicates(src_file):
-    collected_params = {}
-    pass
+def get_paramgroups(src_file, encoding=None):
+    spgroups, sparams = read_entries(src_file, encoding=encoding)
+    return spgroups
 
 
-# def is_duplicate(param, attr='guid'):
-#     comp_attr = getattr(param, attr)
-#     if comp_attr in collected_params.keys():
-#         return True
-#     else:
-#         collected_params[comp_attr] = param
-#
-#     return False
-#
-#
+def get_params(src_file, encoding=None, groupid=None):
+    spgroups, sparams = read_entries(src_file, encoding=encoding)
+    if groupid:
+        return [x for x in sparams if x.group.guid == groupid]
+
+    return sparams
+
+
+def find_duplicates(src_file, encoding=None, byname=False):
+    param_guid_lut = defaultdict(list)
+    group_guid_lut = defaultdict(list)
+
+    spgroups, sparams = read_entries(src_file, encoding=encoding)
+
+    duplparam = 'name' if byname else 'guid'
+
+    for sparam in sparams:
+        param_guid_lut[getattr(sparam, duplparam)].append(sparam)
+
+    for spgroup in spgroups:
+        group_guid_lut[getattr(spgroup, duplparam)].append(spgroup)
+
+    duplgroups = [v for k, v in group_guid_lut.items() if len(v) > 1]
+    duplparams = [v for k, v in param_guid_lut.items() if len(v) > 1]
+
+    return SharedParamEntries(duplgroups, duplparams)
+
+
+def find(src_file, searchstr, encoding=None):
+    spgroups, sparams = read_entries(src_file, encoding=encoding)
+    matchedgroups = [x for x in spgroups if searchstr in x]
+    matchedparams = [x for x in sparams if searchstr in x]
+
+    return SharedParamEntries(matchedgroups, matchedparams)
+
+
+def compare(first_file, second_file, encoding=None):
+    spgroups1, sparams1 = read_entries(first_file, encoding=encoding)
+    spgroups2, sparams2 = read_entries(second_file, encoding=encoding)
+
+    uniqgroups1 = [x for x in spgroups1 if x not in spgroups2]
+    uniqparams1 = [x for x in sparams1 if x not in sparams2]
+    uniqgroups2 = [x for x in spgroups2 if x not in spgroups1]
+    uniqparams2 = [x for x in sparams2 if x not in sparams1]
+
+    return SharedParamEntries(uniqgroups1, uniqparams1), \
+        SharedParamEntries(uniqgroups2, uniqparams2)
